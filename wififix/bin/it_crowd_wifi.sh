76a9cbea8b8c2ee4efaf869c412cfcb274205052
Simon Wingrove <simonjw@gmail.com>
2019-11-26 12:00:39 +0000

    Fix WiFi restarting script to allow apple watch unlock

--- wififix/bin/it_crowd_wifi.sh
+++ wififix/bin/it_crowd_wifi.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 now=$(date +"%T")
+syslog -s -l info "5 second sleep to allow apple watch to unlock macbook"
+sleep 5
 syslog -s -l notice "Turning WiFi off and on again"
 echo "Fixing MacOS stupidity at $now"
 networksetup -setairportpower airport off
